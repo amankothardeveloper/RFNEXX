9b11ab87652dca16770a6fddfb56c86dfe60eb84
amankotharideveloper <amankothari.developer@gmail.com>
2024-07-10 11:41:33 +0530

    Update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,5 +18,49 @@ $(document).ready(function () {
     $(window).scroll(function () {
         headerScrollState();
     });
+
+
+
+    // Home Page Clients Logo Section -----------------------
+    $('#clients-logo').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 8000,
+        pauseOnHover: false,
+        cssEase: 'linear',
+        arrows: false
+    });
 });
 
+
+let counted = 0;
+$(window).scroll(function () {
+    let expConter = document.getElementById("exp-counter");
+    if (expConter) {
+        let oTop = $('#exp-counter').offset().top - window.innerHeight;
+        if (counted == 0 && $(window).scrollTop() > oTop) {
+            $('#exp-counter').each(function () {
+                let $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                    {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                        }
+                    });
+            });
+            counted = 1;
+        }
+    }
+});
