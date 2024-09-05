6887bd501ae692da9586623b226e4ab0c4714536
amankotharideveloper <amankothari.developer@gmail.com>
2024-09-05 12:30:48 +0530

    changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     let windowHeight = $(".hero-banner-sec").outerHeight() - $(".site_header").outerHeight();
     function headerScrollState() {
         let scroll = $(window).scrollTop();
-        console.log(scroll, "==================", windowHeight);
         if (scroll >= 10 && scroll < windowHeight) {
             $(".site_header").addClass("actived");
         } else {
@@ -64,3 +63,4 @@ $(window).scroll(function () {
         }
     }
 });
+
